0d4b9150de4883048c411f9cbb94ab8aa974c39f
user <user@LEENASRI>
2023-11-04 16:41:47 +0530

    Add project files.

---
+++ FoodMenu.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consol
+{
+    public class FoodMenu
+    {
+        private Dictionary<string, Dictionary<string, double>> weeklyMenu;
+
+        public FoodMenu()
+        {
+            weeklyMenu = new Dictionary<string, Dictionary<string, double>>
+        {
+            { "monday", new Dictionary<string, double>
+                {
+                    { "rice", 25 },
+                    { "dal", 20 },
+                    { "chapathi", 15 }
+            }
+            },
+             { "tuesday", new Dictionary<string, double>
+                {
+                    // Tuesday menu items and prices
+                     { "rice", 25},
+                    { "tomato", 20 },
+                    { "dosa", 20 }
+                }
+            },
+            { "wednesday", new Dictionary<string, double>
+                {
+                    // Wednesday menu items and prices
+                     { "rice", 25},
+                    { "potato", 20 },
+                    { "puri", 25 }
+                }
+            },
+                        { "thursday", new Dictionary<string, double>
+                {
+                    // Thursday menu items and prices
+                    { "rice", 25},
+                    { "eggCurry", 20 },
+                    { "idle", 25 }
+                }
+            },
+
+            { "friday", new Dictionary<string, double>
+                {
+                    // Friday menu items and prices
+                     { "rice", 10.99 },
+                    { "dal", 8.49 },
+                    { "chapathi", 12.79 },
+                {"sweet",25 }
+                }
+            }
+        };
+        }
+        public Dictionary<string, double> GetMenu(string day)
+        {
+            if (weeklyMenu.ContainsKey(day))
+            {
+                return weeklyMenu[day];
+            }
+            else
+            {
+
+                throw new ArgumentException("Invalid day input. Please enter a valid day (Monday to Friday).");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consol
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FoodMenu menu = new FoodMenu();
+            double totalCost = 0.0;
+            Dictionary<string, double> selectedItems = new Dictionary<string, double>();
+
+            Console.WriteLine("-----------------Welcome to the Canteen Food Menu System!--------------------");
+            Console.WriteLine(" Please enter employee name: ");
+            string employeeName = Console.ReadLine();
+            Console.WriteLine("Please enter your employee department: ");
+            string department = Console.ReadLine();
+
+            Console.WriteLine(".............Please select a day (Monday to Friday):............");
+            string selectedDay = Console.ReadLine().ToLower();
+
+            Dictionary<string, double> foodItems = menu.GetMenu(selectedDay);
+
+            Console.WriteLine($" Food Menu for - {selectedDay} :");
+            Console.WriteLine("--FoodItem - Price--");
+            foreach (var item in foodItems)
+            {
+                Console.WriteLine($"{item.Key} : ${item.Value}");
+            }
+            while (true)
+            {
+                Console.WriteLine("Please select a food item or type 'exit' to finish:");
+                string selectedItem = Console.ReadLine().ToLower();
+
+                if (selectedItem.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                if (foodItems.ContainsKey(selectedItem))
+                {
+                    selectedItems[selectedItem] = foodItems[selectedItem];
+                    totalCost += foodItems[selectedItem];
+                    Console.WriteLine($"{selectedItem} added to the order. Total cost: ${totalCost}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid food item. Please try again.");
+                }
+            }
+
+            Console.WriteLine("------------------------------ Receipt:----------------------------");
+            Console.WriteLine($"Employee Name: {employeeName}");
+            Console.WriteLine($"Department: {department}");
+            Console.WriteLine("Selected Items:");
+            foreach (var item in selectedItems)
+            {
+                Console.WriteLine($"{item.Key}: ${item.Value}");
+            }
+            Console.WriteLine($"Total Cost: ${totalCost}");
+            Console.ReadLine();
+        }
+    }
+}
